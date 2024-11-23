b92c2b7d8ff7d4851aa78bd30947da864e62aa2f
Erwin Darsono <erwindarsono08@gmail.com>
2024-11-23 23:12:46 +0700

    rwx OK

--- App.java
+++ App.java
@@ -1,85 +1,132 @@
 import java.io.File;
-import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.AclEntry;
-import java.nio.file.attribute.AclEntryType;
+import java.nio.file.attribute.AclEntryFlag;
 import java.nio.file.attribute.AclEntryPermission;
+import java.nio.file.attribute.AclEntryType;
 import java.nio.file.attribute.AclFileAttributeView;
 import java.nio.file.attribute.UserPrincipal;
 import java.util.List;
+import java.util.Set;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        File file = new File("test.txt");
+        Scanner sc = new Scanner(System.in);
+        File file = new File("folder-1");
 
-        if(file.exists()){
-            System.out.println("ada");
+        if (file.exists()) {
+            System.out.println("File exists\n");
+        } else {
+            System.out.println("File not exists");
+            sc.close();
+            return;
+        }   
+
+        Path filePath = Paths.get(file.getName());
+        // printAcl(filePath);
+
+        // String user = "car";
+        System.out.print("User : ");
+        String user = sc.next();
+        try{
+            setAcl(filePath, user);
+            if(Files.isDirectory(filePath)){
+                setAclRecursively(filePath, user);
+            }
+            System.out.println("==ACL set==");
+        } catch(Exception e){
+            System.out.println("Error: " + e.getMessage());
         }
-        else{
-            System.out.println("tak ada");
-        }
+        sc.close();
+    }
 
-        // Specify the relative or absolute path to the file
-        Path filePath = Paths.get("test.txt");  // If it's in the same directory, use a relative path
+    public static void printAcl(Path filePath){
+        AclFileAttributeView view = Files.getFileAttributeView(filePath, AclFileAttributeView.class);
 
-
-
-        try {
-            // // Get the AclFileAttributeView of the file
-            // AclFileAttributeView aclAttr = Files.getFileAttributeView(filePath, AclFileAttributeView.class);
-            
-            // if (aclAttr == null) {
-            //     System.out.println("ACL view is not supported on this file system.");
-            //     return;
-            // }
-
-            // // Get the ACL entries of the file
-            // List<AclEntry> aclEntries = aclAttr.getAcl();
-
-            // Check if there are any ACL entries and print them
-            // if (aclEntries.isEmpty()) {
-            //     System.out.println("No ACL entries found for this file.");
-            // } else {
-            //     System.out.println("ACL entries for the file '" + filePath + "':");
-            //     for (AclEntry entry : aclEntries) {
-            //         System.out.println(entry);
-            //     }
-            // }
-
-            /////////////////////////////////
-            // lookup "joe"
-            UserPrincipal car = filePath.getFileSystem().getUserPrincipalLookupService()
-            .lookupPrincipalByName("car");
-
-            // get view
-            AclFileAttributeView view = Files.getFileAttributeView(filePath, AclFileAttributeView.class);
-
-            // create ACE to give "joe" read access
-            AclEntry entry = AclEntry.newBuilder()
-                .setType(AclEntryType.ALLOW)
-                .setPrincipal(car)
-                .setPermissions(AclEntryPermission.READ_DATA, AclEntryPermission.READ_NAMED_ATTRS, AclEntryPermission.READ_ATTRIBUTES, AclEntryPermission.READ_ACL)
-                .build();
-
-            // read ACL, insert ACE, re-write ACL
+        try{
             List<AclEntry> acl = view.getAcl();
-            acl.add(0, entry);   // insert before any DENY entries
-            view.setAcl(acl);
 
             if (acl.isEmpty()) {
                 System.out.println("No ACL entries found for this file.");
             } else {
                 System.out.println("ACL entries for the file '" + filePath + "':");
-                for (AclEntry entryhehe : acl) {
-                    System.out.println(entryhehe);
+                int idx = 1;
+                for (AclEntry entry : acl) {
+                    System.out.println(idx + ". " +entry);
+                    idx++;
                 }
             }
-            
-
-        } catch (IOException e) {
-            System.err.println("Error reading ACL entries: " + e.getMessage());
+        }
+        catch(Exception e){
+            e.getMessage();
         }
     }
+
+    public static void setAcl(Path filePath, String user) throws Exception{
+        UserPrincipal userPrincipal = filePath.getFileSystem().getUserPrincipalLookupService().lookupPrincipalByName(user);
+
+        AclFileAttributeView aclView = Files.getFileAttributeView(filePath, AclFileAttributeView.class);
+
+        AclEntry aclEntry = AclEntry.newBuilder()
+            .setType(AclEntryType.ALLOW)
+            .setPrincipal(userPrincipal)
+            .setFlags(AclEntryFlag.DIRECTORY_INHERIT, AclEntryFlag.FILE_INHERIT)
+            // .setPermissions(readPermissions)
+            // .setPermissions(writePermissions)
+            // .setPermissions(executePermissions)
+            .setPermissions(rwxPermissions)
+            .build();
+
+        List<AclEntry> acl = aclView.getAcl();
+        acl.add(0, aclEntry);
+        aclView.setAcl(acl);
+    }
+
+    public static void setAclRecursively(Path filePath, String user) throws Exception{
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(filePath)) {
+            for (Path entry: stream) {
+                setAcl(entry,user);
+            }
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    static Set<AclEntryPermission> readPermissions = Set.of(
+        AclEntryPermission.READ_DATA, 
+        AclEntryPermission.READ_ACL, 
+        AclEntryPermission.READ_ATTRIBUTES, 
+        AclEntryPermission.READ_NAMED_ATTRS
+    );
+
+    static Set<AclEntryPermission> writePermissions = Set.of(
+        AclEntryPermission.WRITE_DATA, 
+        AclEntryPermission.APPEND_DATA, 
+        AclEntryPermission.WRITE_ATTRIBUTES, 
+        AclEntryPermission.WRITE_NAMED_ATTRS,
+        AclEntryPermission.DELETE,
+        AclEntryPermission.DELETE_CHILD
+    );
+
+    static Set<AclEntryPermission> executePermissions = Set.of(
+        AclEntryPermission.EXECUTE
+    );
+
+    static Set<AclEntryPermission> rwxPermissions = Set.of(
+        AclEntryPermission.READ_DATA, 
+        AclEntryPermission.READ_ACL, 
+        AclEntryPermission.READ_ATTRIBUTES, 
+        AclEntryPermission.READ_NAMED_ATTRS,
+        AclEntryPermission.WRITE_DATA, 
+        AclEntryPermission.APPEND_DATA, 
+        AclEntryPermission.WRITE_ATTRIBUTES, 
+        AclEntryPermission.WRITE_NAMED_ATTRS,
+        AclEntryPermission.DELETE,
+        AclEntryPermission.DELETE_CHILD,
+        AclEntryPermission.EXECUTE
+    );
 }
---
+++ script.bat
@@ -0,0 +1,10 @@
+@echo off
+REM Create the folder
+mkdir folder-1
+
+REM Navigate to the folder
+cd folder-1
+
+REM Create tes1.txt and tes2.txt
+echo This is tes1.txt > tes1.txt
+echo This is tes2.txt > tes2.txt
